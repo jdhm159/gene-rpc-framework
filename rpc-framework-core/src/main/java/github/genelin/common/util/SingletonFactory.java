4b5c3aea5be143cd9a3499ef21f02eedfc89d598
GeneLin <17jylin7@stu.edu.cn>
2021-01-07 14:21:24 +0800

    feat: add util class SingletonFactory.java   plus-SingletonFactoryTest

---
+++ rpc-framework-core/src/main/java/github/genelin/common/util/SingletonFactory.java
@@ -0,0 +1,62 @@
+package github.genelin.common.util;
+
+import github.genelin.common.entity.Holder;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 单例模式 结合 工厂模式
+ * 统一管理单例对象，进行解耦，简化单例类的编写（创建及暴露单例统一由工厂来负责，无需在每个单例类中编写）
+ * 用于构建单例对象的工厂类（本身也是单例的，使用静态内部类方式）
+ * @author gene lin
+ * @createTime 2020/12/18 19:45
+ */
+public final class SingletonFactory {
+
+    private final ConcurrentHashMap<String, Holder<Object>> OBJECT_MAP;
+
+    // 私有化构造方法
+    private SingletonFactory(ConcurrentHashMap<String, Holder<Object>> map){
+        this.OBJECT_MAP = map;
+    }
+
+    // 静态内部类
+    private static class FactorySingletonHolder{
+        private static SingletonFactory singletonFactory = new SingletonFactory(new ConcurrentHashMap<>());
+    }
+
+    private static SingletonFactory getInstance(){
+        return FactorySingletonHolder.singletonFactory;
+    }
+
+    public static <T> T getSingletonObject(Class<T> clazz){
+        String key = clazz.getName();
+        ConcurrentHashMap<String, Holder<Object>> holderMap = getInstance().OBJECT_MAP;
+        Holder<Object> holder = holderMap.get(key);
+        if (holder == null){
+            holderMap.putIfAbsent(key, new Holder<>());
+            holder = holderMap.get(key);
+        }
+
+        Object obj = holder.get();
+        if (obj == null){
+            synchronized (holder){
+                obj = holder.get();
+                if (obj == null){
+                    try {
+                        Constructor<T> c = clazz.getDeclaredConstructor();
+                        c.setAccessible(true);
+                        obj = c.newInstance();
+                        holder.set(obj);
+                    } catch (InstantiationException | IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    } catch (NoSuchMethodException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return clazz.cast(obj);
+    }
+}
---
+++ rpc-framework-core/src/test/java/SingletonFactoryTest.java
@@ -0,0 +1,26 @@
+import github.genelin.common.util.SingletonFactory;
+import github.genelin.remoting.transport.ServiceProviderImpl;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author gene lin
+ * @createTime 2021/1/7 13:40
+ */
+public class SingletonFactoryTest {
+
+    @Test
+    public void getSingletonInstanceTest(){
+        Object singletonObject = SingletonFactory.getSingletonObject(Object.class);
+        Object singletonObject2 = SingletonFactory.getSingletonObject(Object.class);
+        Assert.assertEquals(singletonObject, singletonObject2);
+    }
+
+    @Test
+    public void getSingletonServiceProvider(){
+        ServiceProviderImpl serviceProvider = SingletonFactory.getSingletonObject(ServiceProviderImpl.class);
+        ServiceProviderImpl serviceProvider2 = SingletonFactory.getSingletonObject(ServiceProviderImpl.class);
+        Assert.assertNotNull(serviceProvider);
+        Assert.assertEquals(serviceProvider, serviceProvider2);
+    }
+}
